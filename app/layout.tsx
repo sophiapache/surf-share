330abefa52a42c4ee7f8cb69f98271ab8c23a9af
Sophia Pache <sophpache@gmail.com>
2024-02-08 15:43:10 -0500

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 import { Inter } from "next/font/google";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
