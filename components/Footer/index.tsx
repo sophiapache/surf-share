46c9ebad284df13f27b23cf652e219de396821e4
Sophia Pache <sophpache@gmail.com>
2025-04-06 20:36:06 -0400

    adding functionality to page

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,5 +1,24 @@
+"use client";
+import useSWR from "swr";
+
+const fetcher = async (url: string) => fetch(url).then((res) => res.json());
+
 const Footer = () => {
-  return <div></div>;
+  const { data, mutate } = useSWR<{ id: string }[]>("/api/id", fetcher);
+
+  return (
+    <div
+      onClick={async () => {
+        await fetch("/api/id", {
+          method: "POST",
+        });
+        await mutate();
+      }}
+    >
+      CLICK HERE
+    </div>
+  );
+  <div>{data ? data?.map(({ id }) => <p key={id}>{id}</p>) : null} </div>;
 };
 
 export default Footer;
